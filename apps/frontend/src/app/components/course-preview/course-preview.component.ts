8a7d3944a48ea129fa6d8064fe22940876269fe7
Horatzio <horatiuduma@gmail.com>
2022-05-12 13:10:23 +0300

    course preview component on course-list page.

--- apps/frontend/src/app/components/course-preview/course-preview.component.ts
+++ apps/frontend/src/app/components/course-preview/course-preview.component.ts
@@ -11,21 +11,22 @@ import { AuthService } from '../../shared/services/auth.service';
 @Component({
   selector: 'app-course-preview',
   templateUrl: './course-preview.component.html',
-  styleUrls: ['./course-preview.component.scss']
+  styleUrls: ['./course-preview.component.scss'],
 })
 export class CoursePreviewComponent implements OnInit {
   @Input('course') public course!: Course;
-  @Input('courseName') public courseName!: string;
   private courseService: EntityService<Course>;
- 
-  constructor(public router: Router,factory: EntityServiceFactory, public authService: AuthService) {
+
+  constructor(
+    public router: Router,
+    factory: EntityServiceFactory,
+    public authService: AuthService
+  ) {
     this.courseService = factory.create(Collections.Course);
   }
- 
+
   ngOnInit(): void {
-    
     this.courseService.getAll().subscribe((c) => (this.courses = c));
-    
   }
   public goToCourse() {
     this.router.navigate(['course', this.course.id.toString()]);
